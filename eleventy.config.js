8db9e6717db3cd9c785222d6e0abf14065d4ae91
Chris Burgess <chris@giantrobot.co.nz>
2025-10-24 22:11:29 +1300

    Config tweaks

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,19 @@
 export default function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/media", {
-        // overwrite: false
-    });
-
-    return {
+    const config = {
         dir: {
             input: "src",
             output: "public"
         }
     };
+
+    // Better to get this path at runtime, eg from process.env.GITHUB_REPOSITORY ?
+    if (process.env['GITHUB_ACTIONS']) {
+        config['pathPrefix'] = '11ty-sandbox';
+    }
+
+    eleventyConfig.addPassthroughCopy("src/media", {
+        // overwrite: false
+    });
+
+    return config;
 };
