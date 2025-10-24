4d1e8a2a4f73b75c41b3c49fff28f5ef37066f54
Chris Burgess <chris@giantrobot.co.nz>
2025-10-25 07:29:08 +1300

    Nicer curl output

--- bin/fetch-media.sh
+++ bin/fetch-media.sh
@@ -3,5 +3,5 @@
 set -e
 
 # Fetches media and extracts to src/media
-curl -o media.tgz https://chris.bur.gs/media/media.tgz
+curl --silent --output media.tgz https://chris.bur.gs/media/media.tgz
 tar -xzvf media.tgz
